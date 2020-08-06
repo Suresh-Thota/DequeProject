86f0ef7d51bf6f32cf6aea61537d8bfab36ad654
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-06 20:57:15 +0530

    Add project files.

---
+++ Deque/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Deque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice, x;
+            DequeA dq = new DequeA(8);
+            while (true)
+            {
+                Console.WriteLine("1.Insert at the front end");
+                Console.WriteLine("2.Insert at the rear end");
+                Console.WriteLine("3.Delete from the front end");
+                Console.WriteLine("4.Delete from the rear end");
+                Console.WriteLine("5.Display all elements from the deque");
+                Console.WriteLine("6.quit");
+                Console.Write("enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 6)
+                {
+                    break;
+                }
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("enter an Element");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        dq.InserFront(x);
+                        break;
+                    case 2:
+                        Console.Write("enter an Element");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        dq.InsertRear(x);
+                        break;
+                    case 3:
+                        x = dq.DeleteFront();
+                        Console.Write("deleted element is :" + x);
+                        break;
+                    case 4:
+                        x = dq.DeleteFront();
+                        Console.Write("deleted element is :" + x);
+                        break;
+                    case 5:
+                        dq.Display();
+                        break;
+                    default:
+                        Console.Write("wring choice");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
